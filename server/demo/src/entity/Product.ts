897774b238cb3a21567deb4b6a7ebcb6ee4116e3
yhiffy <han_yan1@brown.edu>
2024-01-11 18:20:01 -0400

    update

--- server/demo/src/entity/Product.ts
+++ server/demo/src/entity/Product.ts
@@ -1,37 +1,47 @@
-import { Entity as entity, Column } from 'typeorm';
-import {IsDecimal, IsInt, IsPositive, Min, MinLength} from "class-validator";
+import {Entity as entity, Column, ManyToMany, ManyToOne} from 'typeorm';
+import {IsDecimal, IsInt, IsPositive, Length, Min, MinLength} from "class-validator";
 import BaseClass from "./BaseClass";
+import {User} from "./User";
 @entity('product')
 class Product extends BaseClass {
+    @Column()
+    @Length(1)
+    id: string
+
+    @Column()
+    @Length(1, 200)
+    symbol: string
+
     @Column()
     @MinLength(1)
     name: string;
 
-    @Column({
-        type: 'decimal',
-        precision: 10,
-        scale: 2
-    })
-    @IsDecimal()
-    price: string;
-
-    @Column({nullable:false, default:1})
-    @IsInt()
-    @IsPositive()
-    store: number;
+    @Column()
+    @Length(1, 200)
+    image: string
 
     @Column()
-    description: string;
+    @Min(0)
+    currentPrice: number
+
+    @Column({
+        nullable: false,
+        default:1
+    })
+    @Min(0)
+    marketCap: number
 
     @Column()
     @IsInt()
     @Min(1)
-    rank: number;
+    marketCapRank: number
 
     @Column()
-    img: string;
+    @IsInt()
+    @Min(0)
+    totalVolume: number
 
-
+    // @ManyToOne(() => User, user => user.orders)
 }
 
 export default Product;
