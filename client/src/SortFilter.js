79f9eb0d799a6c971614b774e864829c1df806c2
yhiffy <han_yan1@brown.edu>
2024-01-14 22:47:42 -0400

    update sortByPrice ASC | DESC

--- client/src/SortFilter.js
+++ client/src/SortFilter.js
@@ -3,14 +3,21 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import {useDispatch} from "react-redux";
+import {sortProductsByPrice} from "./redux/features/productSlice.js";
 
 export default function SelectAutoWidth() {
     const [sort, setSort] = React.useState('');
+    const dispatch = useDispatch()
 
     const createHandleMenuClick = (menuItem) => {
         setSort(menuItem.target.value)
             console.log(`Click on`, menuItem.target.value)
     };
+
+    // const sortByPrice = (menuItem) => {
+    //     dispatch(sortProductsByPrice(menuItem))
+    // }
 
     return (
         <div>
@@ -27,9 +34,9 @@ export default function SelectAutoWidth() {
                     <MenuItem value="">
                         <em>None</em>
                     </MenuItem>
-                    <MenuItem value={'DESC'}>Price (High to Low)</MenuItem>
-                    <MenuItem value={'ASC'}>Price (Low to High)</MenuItem>
-                    <MenuItem value={'Language settings'}>Initial Letter</MenuItem>
+                    <MenuItem value={'DESC'} onClick={() => dispatch(sortProductsByPrice('DESC'))}>Price (High to Low)</MenuItem>
+                    <MenuItem value={'ASC'} onClick={() => dispatch(sortProductsByPrice('ASC'))}>Price (Low to High)</MenuItem>
+                    <MenuItem value={'Letter'}>Initial Letter</MenuItem>
                     <MenuItem value={'Rank'}>Market Cap Rank</MenuItem>
                 </Select>
             </FormControl>
